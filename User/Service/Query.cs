bcfe796266b72497f242d5899b4ad394a27eae31
Mahr <mahirmuzahid@gmail.com>
2023-03-28 00:48:10 +0600

    Role Based AUTH Done

--- User/Service/Query.cs
+++ User/Service/Query.cs
@@ -7,14 +7,14 @@ namespace UserService.Service
 {
     public class Query
     {
-        
+
         [UseProjection]
-        [Authorize]
+        [Authorize(Roles = new string[] { "User" })]
         public async Task<List<SharedModal.Modals.User>> GetUser([Service] DataContext _context)
         {
             return  await _context.Users.Include(u => u.City).ToListAsync();          
         }
-        [Authorize]
+        [Authorize(Roles = new string[] { "User" })]
         [UseProjection]
         public async Task<SharedModal.Modals.User> GetUserByID([Service] DataContext _context, int UserID)
         {
